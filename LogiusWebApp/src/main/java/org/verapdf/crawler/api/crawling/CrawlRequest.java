2af0d73fdcdac3d302c09132a2a79184769157dd
Maksim Bezrukov <maksim.bezrukov@duallab.com>
2017-09-15 13:46:11 +0300

    Some minor fixes

--- LogiusWebApp/src/main/java/org/verapdf/crawler/api/crawling/CrawlRequest.java
+++ LogiusWebApp/src/main/java/org/verapdf/crawler/api/crawling/CrawlRequest.java
@@ -1,6 +1,5 @@
 package org.verapdf.crawler.api.crawling;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.hibernate.validator.constraints.NotEmpty;
