14ea1d98165eccd3a7fe3da6cab5910188cbff8a
evgeny <EvgenyChermenin@gmail.com>
2019-01-15 02:30:13 +0300

    change packages name

---
+++ LogiusWebApp/src/main/java/com/verapdf/crawler/logius/app/report/CrawlJobSummary.java
@@ -0,0 +1,35 @@
+package com.verapdf.crawler.logius.app.report;
+
+import com.verapdf.crawler.logius.app.document.DomainDocument;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CrawlJobSummary {
+
+    private Map<DomainDocument.DocumentTypeGroup, Long> openDocuments;
+
+    private Map<DomainDocument.DocumentTypeGroup, Long> notOpenDocuments;
+
+    public Map<DomainDocument.DocumentTypeGroup, Long> getOpenDocuments() {
+        if (openDocuments == null) {
+            openDocuments = new HashMap<>();
+        }
+        return openDocuments;
+    }
+
+    public void setOpenDocuments(Map<DomainDocument.DocumentTypeGroup, Long> openDocuments) {
+        this.openDocuments = openDocuments;
+    }
+
+    public Map<DomainDocument.DocumentTypeGroup, Long> getNotOpenDocuments() {
+        if (notOpenDocuments == null) {
+            notOpenDocuments = new HashMap<>();
+        }
+        return notOpenDocuments;
+    }
+
+    public void setNotOpenDocuments(Map<DomainDocument.DocumentTypeGroup, Long> notOpenDocuments) {
+        this.notOpenDocuments = notOpenDocuments;
+    }
+}
