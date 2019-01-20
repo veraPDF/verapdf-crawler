f6ecd70cadef575dd9b847f87e659b529bcf163d
Evgeny <evgeny.chermenin@duallab.com>
2019-01-20 19:47:42 +0300

    add @EnableAsync

--- LogiusWebApp/src/main/java/org/verapdf/crawler/logius/LogiusWebApplication.java
+++ LogiusWebApp/src/main/java/org/verapdf/crawler/logius/LogiusWebApplication.java
@@ -1,20 +1,19 @@
-package com.verapdf.crawler.logius.app;
+package org.verapdf.crawler.logius;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 
 @SpringBootApplication(exclude = {
         DataSourceAutoConfiguration.class,
         DataSourceTransactionManagerAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class })
+        HibernateJpaAutoConfiguration.class})
+@EnableAsync
 public class LogiusWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(LogiusWebApplication.class, args);
